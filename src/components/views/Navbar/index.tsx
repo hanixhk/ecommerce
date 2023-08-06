a3172541d93c10e50c9bc5b77ad01ebbd6fb53d1
hanixhk <abdulhannanxhk@gmail.com>
2023-08-06 13:12:37 -0700

    some errors resolved

--- src/components/views/Navbar/index.tsx
+++ src/components/views/Navbar/index.tsx
@@ -24,7 +24,7 @@ const Navbar = () => {
             {NavbarArray.map((item:NavbarItemType,index:number)=>(
              <li key={index} className="flex items-center relative border rounded-sm px-3 py-1 hover:bg-slate-500 cursor-pointer group">
                 <Link  href= {item.href} >{item.label}</Link>
-                {item.isDropDown? <HiOutlineChevronDown className="mt-1 rotate-180 group-hover:rotate-0" size={16}/>:""}
+                {item.isDropDown? <HiOutlineChevronDown  size={16} />:""}
                 {item.isDropDown &&<div className={"invisible group-hover:visible absolute top-8 left-0 py-2 px-6 bg-gray-100 font-light min-w-[7.8rem]"}>
             <DropDown item={item}/>
             </div>}

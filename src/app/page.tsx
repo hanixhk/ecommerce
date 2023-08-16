12b0a044c1fbc1a929a5361ed34190b757809e69
hanixhk <abdulhannanxhk@gmail.com>
2023-08-15 19:53:47 -0700

    some errors are resolved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,15 @@
 import Image from 'next/image'
 import Hero from '../components/views/Hero'
 import ProductsType from '@/components/views/Product Types'
-// import BASE_PATH_FORAPI from '@/components/shared/BasePath';
+ import BASE_PATH_FORAPI from '@/components/shared/BasePath'
 import ProductCarousel from '@/components/views/ProductCarousel';
 import { responseType } from '@/components/utils/ProductsDataArrayAndType';
 async function fetchAllProductsData() {
-  // let res = await fetch(`${BASE_PATH_FORAPI}/api/products`); 
-  // if (!res.ok) {
-  //   throw new Error("Failed to fetch")
-  // }
-  // return res.json();
-  return{response:"HI"}
+  let res = await fetch(`${BASE_PATH_FORAPI}/api/products`); 
+  if (!res.ok) {
+    throw new Error("Failed to fetch")
+  }
+  return res.json();
 }
 export default async function Home() {
   let {response} =await fetchAllProductsData();

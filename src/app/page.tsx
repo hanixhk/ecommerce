3c4eeb333f75ffeba04f4cfc62b324f8dfc15c16
hanixhk <abdulhannanxhk@gmail.com>
2023-08-15 18:36:31 -0700

    some errors are resolved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,17 @@ import Hero from '../components/views/Hero'
 import ProductsType from '@/components/views/Product Types'
 import BASE_PATH_FORAPI from '@/components/shared/BasePath';
 import ProductCarousel from '@/components/views/ProductCarousel';
-// import { responseType } from '@/components/utils/ProductsDataArrayAndType';
+import { responseType } from '@/components/utils/ProductsDataArrayAndType';
 async function fetchAllProductsData() {
-  // let res = await fetch(`${BASE_PATH_FORAPI}/api/products`); 
-  // if (!res.ok) {
-  //   throw new Error("Failed to fetch")
-  // }
-  // return res.json();
-  return {response:"HI"}
+  let res = await fetch(`${BASE_PATH_FORAPI}/api/products`); 
+  if (!res.ok) {
+    throw new Error("Failed to fetch")
+  }
+  return res.json();
 }
 export default async function Home() {
-  let { response } =await fetchAllProductsData();
+  let {response} =await fetchAllProductsData();
+  // console.log("res:",response)
   return (
    <div>
     <Hero /> 

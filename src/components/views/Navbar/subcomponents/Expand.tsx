5c8d733b312ccc8bbf230956f55844bfd57e2bce
hanixhk <abdulhannanxhk@gmail.com>
2023-08-15 19:33:08 -0700

    some errors are resolved

--- src/components/views/Navbar/subcomponents/Expand.tsx
+++ src/components/views/Navbar/subcomponents/Expand.tsx
@@ -22,7 +22,7 @@ const Expand: FC<{ item: NavbarItemType }> = ({ item }) => {
                 {item.isDropDown ? <HiOutlineChevronDown className="mt-1 -rotate-180 group-hover:rotate-0 duration-300" size={15} /> : ""}
             </div>
             <div className="flex flex-col space-y-1 mt-2">
-                {isTimeOut && item.dropDownData?.map((subItem: NavbarItemType, index: number) => (
+                {isTimeOut && item.dropDowndata?.map((subItem: NavbarItemType, index: number) => (
                     <Link key={index} className="hover:bg-gray-50 rounded-md py-1 px-5 duration-300 " href={subItem.href}>
                         {subItem.label}
                     </Link>

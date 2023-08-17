729a2863aff4e45323351fdbcede9f839f8a9557
hanixhk <abdulhannanxhk@gmail.com>
2023-08-16 18:53:41 -0700

    some errors resolved

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import Navbar from '@/components/views/Navbar'
 import './globals.css'
 import { Maven_Pro } from 'next/font/google'
 import Wrapper from '@/components/shared/Wrapper'
+import Footer from '@/components/views/Footer'
 const inter = Maven_Pro({
   subsets: ['latin'],
   weight: ["400", "500", "600","700","800","900"]
@@ -22,6 +23,7 @@ export default function RootLayout({
       <Wrapper>
         <Navbar />
         {children} 
+        <Footer/>
         </Wrapper>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import ProductsType from '@/components/views/Product Types'
  import BASE_PATH_FORAPI from '@/components/shared/BasePath'
 import ProductCarousel from '@/components/views/ProductCarousel';
 import { responseType ,oneProductType} from '@/components/utils/ProductsDataArrayAndType';
+import Jewellery from '@/components/views/Jewellery';
+import Newslatter from '@/components/views/Newslatter';
 async function fetchAllProductsData() {
   let res = await fetch(`https://${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}.api.sanity.io/v2021-06-07/data/query/production?query=*[_type == "products"]`); 
   if (!res.ok) {
@@ -16,9 +18,11 @@ export default async function Home() {
   return (
    <div>
     
-    {/* <Hero />  */}
-     {/* <ProductsType/> */}
+    <Hero /> 
+     <ProductsType/>
     <ProductCarousel ProductData={result} />
+    <Jewellery />
+    <Newslatter/>
    </div>
     )
 }

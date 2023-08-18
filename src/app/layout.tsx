e8584fa4f28ddb63a50681a43e91081308b1b6f3
hanixhk <abdulhannanxhk@gmail.com>
2023-08-17 20:40:50 -0700

    some errors resolved

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,8 +21,11 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}> 
       <Wrapper>
+        
         <Navbar />
+        <div>
         {children} 
+        </div>
         <Footer/>
         </Wrapper>
       </body>

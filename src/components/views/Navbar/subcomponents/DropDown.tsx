eee770137f038c32f2ee7899f100dd65a5280695
hanixhk <abdulhannanxhk@gmail.com>
2023-08-20 21:16:48 -0700

    some errors resolved

--- src/components/views/Navbar/subcomponents/DropDown.tsx
+++ src/components/views/Navbar/subcomponents/DropDown.tsx
@@ -5,7 +5,7 @@ import { FC } from "react"
 const DropDown: FC<{ item: NavbarItemType }> = ({ item }) => {
     return (
         <ul>
-            {item.dropDowndata?.map((item: NavbarItemType, index: number) => (
+            {item.dropDownData?.map((item: NavbarItemType, index: number) => (
                 <li key={index} className="hover:ml-2 group-hover:duration-300 -translate-y-7 group-hover:translate-y-0">
                     <Link href={item.href}>{item.label}</Link>
                 </li>

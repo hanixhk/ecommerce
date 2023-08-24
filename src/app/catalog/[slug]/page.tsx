6b4e7e2f869d1e4dc25f5229e9a9de9f65901dbc
hanixhk <abdulhannanxhk@gmail.com>
2023-08-23 21:27:00 -0700

    some errors resolved

--- src/app/catalog/[slug]/page.tsx
+++ src/app/catalog/[slug]/page.tsx
@@ -1,8 +1,15 @@
+import { responseType } from '@/components/utils/ProductsDataArrayAndType'
+import ProductDetail from '@/components/views/ProductDetail'
 import  { FC } from 'react'
 
-const Catalog:FC<{params:{slug:string}}> = ({params}) => {
-    return (
-    <div>{params.slug}</div>
+async function fetchPreviewData(slug:string){
+  let res = await fetch(`https://zxyxfzkk.api.sanity.io/v2023-05-26/data/query/production?query=*%5B_type+%3D%3D+%22products%22+%26%26+slug.current%3D%3D%22${slug}%22+%5D&perspective=published`)
+  return res.json()
+}
+const Catalog= async({params}:{params:{slug:string}}) => {
+  let data:responseType = await fetchPreviewData(params.slug)  
+  return (
+    <div><ProductDetail item={data.result[0]}/></div>
   )
 }
 
